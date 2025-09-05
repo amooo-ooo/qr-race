31416015cea62a945ba2ad8f6eac3dd44b7a5ab6
Amor Budiyanto <amor.budiyanto@gmail.com>
2025-09-05 15:13:18 +1200

    fix: styles import

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,5 +1,5 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
-import { Link, ViteClient } from 'vite-ssr-components/hono'
+import { ViteClient } from 'vite-ssr-components/hono'
 import './style.css'
 
 export const renderer = jsxRenderer(({ children }) => {
@@ -9,7 +9,7 @@ export const renderer = jsxRenderer(({ children }) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <ViteClient />
-        <Link href="./src/style.css" rel="stylesheet" />
+        <link href="/style.css" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
